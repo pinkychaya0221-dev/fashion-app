7dfd22062c3c94da3787e4e4bc66f2053d73bcc8
Daichi-prog <takeida0701@gmail.com>
2026-04-28 10:03:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -180,8 +180,11 @@ comment_map = {
     ("winter", "women", "business"): "ニット＋コートで上品な防寒ビジカジ。シルエット重視でスタイルアップ。",
 }
 
+import os
 import random
 import base64
+
+st.title("BGM付きページ")
 
 music_folder = "music"
 
